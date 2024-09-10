1bf0dac47a343e1aac8e32ba00399aa20e41dae4
HillTopTRPG <mihikari90@gmail.com>
2024-09-11 07:54:50 +0900

    ローカルサーバーのhttps対応

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./https/localhost-key.pem'),
+      cert: fs.readFileSync('./https/localhost.pem'),
+    }
+  },
 })
