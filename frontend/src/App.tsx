4678f071e6fd3aeb43ea8f8719e7319fa7232aad
HillTopTRPG <mihikari90@gmail.com>
2024-09-11 13:02:42 +0900

    マッチしないルーティングのページを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.tsx'
+import NotFound from './pages/NotFound.tsx'
 
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   )
 }
